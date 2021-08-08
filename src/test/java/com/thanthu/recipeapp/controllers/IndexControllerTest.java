6083d495b42423d2bd4eb42916140bf04f888ed5
Thanthu <tnair@orthofx.com>
2021-08-08 20:34:53 +0530

    Update IndexControllerTest to verify the Recipes Set argument. Closes #15.

--- src/test/java/com/thanthu/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/thanthu/recipeapp/controllers/IndexControllerTest.java
@@ -5,14 +5,20 @@ import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import com.thanthu.recipeapp.domain.Recipe;
 import com.thanthu.recipeapp.services.RecipeService;
 
 class IndexControllerTest {
@@ -40,13 +46,26 @@ class IndexControllerTest {
 
 	@Test
 	void getIndexPage() {
-		String html = "index";
+		String expectedView = "index";
+		
+		Set<Recipe> recipes = new HashSet<Recipe>();
+		Recipe recipe1 = new Recipe();
+		recipe1.setId(1L);
+		recipes.add(recipe1);
+		Recipe recipe2 = new Recipe();
+		recipe2.setId(2L);
+		recipes.add(recipe2);
+		
+		when(recipeService.getRecipes()).thenReturn(recipes);
+		
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
-		String result = indexController.getIndexPage(model);
+		String actualView = indexController.getIndexPage(model);
 
-		assertEquals(html, result);
+		assertEquals(expectedView, actualView);
 		verify(recipeService, times(1)).getRecipes();
-		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+		assertEquals(recipes, argumentCaptor.getValue());
 	}
 
 }
