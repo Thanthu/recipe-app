8660738f9f6f12c874543f915bddf197ba127667
Thanthu <tnair@orthofx.com>
2021-08-09 08:18:05 +0530

    Created MockMVC Test for IndexController. Closes #16.

--- src/test/java/com/thanthu/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/thanthu/recipeapp/controllers/IndexControllerTest.java
@@ -6,6 +6,9 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -16,6 +19,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import com.thanthu.recipeapp.domain.Recipe;
@@ -47,7 +52,7 @@ class IndexControllerTest {
 	@Test
 	void getIndexPage() {
 		String expectedView = "index";
-		
+
 		Set<Recipe> recipes = new HashSet<Recipe>();
 		Recipe recipe1 = new Recipe();
 		recipe1.setId(1L);
@@ -55,9 +60,9 @@ class IndexControllerTest {
 		Recipe recipe2 = new Recipe();
 		recipe2.setId(2L);
 		recipes.add(recipe2);
-		
+
 		when(recipeService.getRecipes()).thenReturn(recipes);
-		
+
 		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
 		String actualView = indexController.getIndexPage(model);
@@ -66,6 +71,13 @@ class IndexControllerTest {
 		verify(recipeService, times(1)).getRecipes();
 		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
 		assertEquals(recipes, argumentCaptor.getValue());
+	}
+
+	@Test
+	void testMockMvc() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
 	}
 
 }
