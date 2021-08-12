8eb4b55016a04323fd6129d78a80825f0d48d15f
Thanthu <tnair@orthofx.com>
2021-08-12 13:27:28 +0530

    Add New Recipe Feature. Closes #27.

--- src/main/java/com/thanthu/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/thanthu/recipeapp/controllers/RecipeController.java
@@ -2,9 +2,13 @@ package com.thanthu.recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.thanthu.recipeapp.commands.RecipeCommand;
 import com.thanthu.recipeapp.services.RecipeService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +29,20 @@ public class RecipeController {
 		log.debug("GET /recipe/show/" + id);
 		model.addAttribute("recipe", recipeService.getRecipeById(id));
 		return "recipe/show";
+	}
+	
+	@RequestMapping({"/new"})
+	public String newRecipe(Model model) {
+		log.debug("GET /recipe/new");
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/recipeform";
+	}
+	
+	@PostMapping("")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		log.debug("Inside RecipeController.saveOrUpdate()");
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+		return "redirect:/recipe/show/" + savedCommand.getId();
 	}
 
 }
