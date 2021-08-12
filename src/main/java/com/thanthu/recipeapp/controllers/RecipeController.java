7cb7a92c4fd216e7edfcc047cd800672c93d61ee
Thanthu <tnair@orthofx.com>
2021-08-12 14:42:16 +0530

    Create Unit Tests for Add New Recipe Feature feature. Closes #28.

--- src/main/java/com/thanthu/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/thanthu/recipeapp/controllers/RecipeController.java
@@ -42,7 +42,7 @@ public class RecipeController {
 	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
 		log.debug("Inside RecipeController.saveOrUpdate()");
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
-		return "redirect:/recipe/show/" + savedCommand.getId();
+		return "redirect:/recipe/" + savedCommand.getId() + "/show";
 	}
 
 }
--- src/test/java/com/thanthu/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/thanthu/recipeapp/controllers/RecipeControllerTest.java
@@ -1,7 +1,10 @@
 package com.thanthu.recipeapp.controllers;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -11,9 +14,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.thanthu.recipeapp.commands.RecipeCommand;
 import com.thanthu.recipeapp.domain.Recipe;
 import com.thanthu.recipeapp.services.RecipeService;
 
@@ -46,6 +51,26 @@ class RecipeControllerTest {
 		mockMvc.perform(get("/recipe/show/" + RECIPE_ID))
 		.andExpect(status().isOk())
 		.andExpect(view().name("recipe/show"));
+	}
+	
+	@Test
+    public void testGetNewRecipeForm() throws Exception {
+        mockMvc.perform(get("/recipe/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/recipeform"))
+                .andExpect(model().attributeExists("recipe"));
+    }
+
+	@Test
+	public void testPostNewRecipeForm() throws Exception {
+		RecipeCommand command = new RecipeCommand();
+		command.setId(2L);
+
+		when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+		mockMvc.perform(post("/recipe").contentType(MediaType.APPLICATION_FORM_URLENCODED))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(view().name("redirect:/recipe/2/show"));
 	}
 
 }
